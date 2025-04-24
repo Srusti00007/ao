e9c588e373f87551c766de1863165a4a57b960bf
Srusti00007 <srusti007@gmail.com>
2025-04-24 12:28:57 +0530

    savefile

--- demo.java
+++ demo.java
@@ -5,15 +5,11 @@ public class demo {
             int num2 = 10;         
             int sum = num1 + num2;
             int difference = num1 - num2;   
-            int quotient = num1 / num2;
-            int remainder = num1 % num2; 
+            
             System.out.println("Number 1: " + num1);
             System.out.println("Number 2: " + num2);
             System.out.println("Addition: " + sum);
             System.out.println("Subtraction: " + difference);
-            System.out.println("Multiplication: " + product);
-        System.out.println("Division: " + quotient);
-        System.out.println("Remainder: " + remainder);
         }
     }
     
